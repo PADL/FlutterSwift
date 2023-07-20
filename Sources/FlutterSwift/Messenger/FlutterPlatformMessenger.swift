970109c8ff671a6773ae8dbe6630b1800592b3ad
Luke Howard <lukeh@padl.com>
2023-07-20 14:54:00 +0200

    fix platform messenger build failures

--- Sources/FlutterSwift/Messenger/FlutterPlatformMessenger.swift
+++ Sources/FlutterSwift/Messenger/FlutterPlatformMessenger.swift
@@ -14,31 +14,64 @@ import FlutterMacOS
 public actor FlutterPlatformMessenger: FlutterBinaryMessenger {
     #if canImport(Flutter)
     public typealias PlatformFlutterBinaryMessenger = Flutter.FlutterBinaryMessenger
+    public typealias PlatformFlutterBinaryMessageHandler = Flutter.FlutterBinaryMessengerHandler
     #elseif canImport(FlutterMacOS)
     public typealias PlatformFlutterBinaryMessenger = FlutterMacOS.FlutterBinaryMessenger
+    public typealias PlatformFlutterBinaryMessageHandler = FlutterMacOS.FlutterBinaryMessageHandler
     #endif
 
     private let platformBinaryMessenger: FlutterMacOS.FlutterBinaryMessenger
+
+    // MARK: - Initializers
 
     public init(wrapping platformBinaryMessenger: PlatformFlutterBinaryMessenger) {
         self.platformBinaryMessenger = platformBinaryMessenger
     }
 
+    // MARK: - FlutterDesktopMessenger wrappers
+
     @MainActor
-    public func send(on channel: String, message: Data?) async throws {
-        platformBinaryMessenger.send(onChannel: channel, message: message)
+    private func _setMessageHandler(
+        on channel: String,
+        _ binaryMessageHandler: PlatformFlutterBinaryMessageHandler?
+    ) -> FlutterBinaryMessengerConnection {
+        platformBinaryMessenger.setMessageHandlerOnChannel(
+            channel,
+            binaryMessageHandler: binaryMessageHandler
+        )
     }
 
     @MainActor
+    private func _cleanup(connection: FlutterBinaryMessengerConnection) {
+        platformBinaryMessenger.cleanUpConnection(connection)
+    }
+
+    @MainActor
+    public func _send(
+        on channel: String,
+        message: Data?,
+        _ binaryReply: FlutterBinaryReply?
+    ) {
+        platformBinaryMessenger.send(onChannel: channel, message: message, binaryReply: binaryReply)
+    }
+
+    // MARK: - public API
+
+    public func send(on channel: String, message: Data?) async throws {
+        await _send(on: channel, message: message, nil)
+    }
+
     public func send(
         on channel: String,
         message: Data?,
         priority: TaskPriority?
     ) async throws -> Data? {
         try await withPriority(priority) {
-            try await withCheckedThrowingContinuation {
-                platformBinaryMessenger.send(onChannel: channel, message: message) { reply in
-                    reply
+            await withCheckedContinuation { continuation in
+                Task {
+                    await self._send(on: channel, message: message) { binaryReply in
+                        continuation.resume(returning: binaryReply)
+                    }
                 }
             }
         }
@@ -50,20 +83,20 @@ public actor FlutterPlatformMessenger: FlutterBinaryMessenger {
         priority: TaskPriority?
     ) async throws -> FlutterBinaryMessengerConnection {
         guard let handler else {
-            return platformBinaryMessenger.setMessageHandlerOnChannel(
-                channel,
-                binaryMessageHandler: nil
-            )
+            return await _setMessageHandler(on: channel, nil)
         }
-        return platformBinaryMessenger.setMessageHandlerOnChannel(channel) { message, callback in
-            Task(priority: priority) { @MainActor in
-                callback(try await handler(message!))
+        return await _setMessageHandler(on: channel) { message, callback in
+            Task {
+                let response = try await handler(message)
+                DispatchQueue.main.async {
+                    callback(response)
+                }
             }
         }
     }
 
-    public func cleanUp(connection: FlutterBinaryMessengerConnection) throws {
-        platformBinaryMessenger.cleanUpConnection(connection)
+    public func cleanUp(connection: FlutterBinaryMessengerConnection) async throws {
+        await _cleanup(connection: connection)
     }
 }
 #endif
