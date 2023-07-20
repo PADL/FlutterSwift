3d6d2ab2074a8b268a32b5a0c46447c4dc06c532
Luke Howard <lukeh@padl.com>
2023-07-20 19:53:13 +0200

    update LD_LIBRARY_PATH

--- run-test-linux.sh
+++ run-test-linux.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=/opt/elinux/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/flutter-elinux/lib:$LD_LIBRARY_PATH
 swift test -Xswiftc -cxx-interoperability-mode=default
