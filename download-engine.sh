974609a57308ad710e9aa5263fe77f64831c9894
Luke Howard <lukeh@padl.com>
2024-04-12 22:18:34 +1000

    update upstream flutter eLinux

--- download-engine.sh
+++ download-engine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ENGINE_URL="https://github.com/sony/flutter-embedded-linux/releases/download/b20183e040"
+ENGINE_URL="https://github.com/sony/flutter-embedded-linux/releases/download/e76c956498"
 
 tmp_dir=$(mktemp -d -t engine-XXXXXXXXXX)
 artifact_dir="$tmp_dir/flutter-engine.artifactbundle"
