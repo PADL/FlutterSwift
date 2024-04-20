b920d95a0842847cfe1325e042bc47f009ee4aa0
Luke Howard <lukeh@padl.com>
2024-04-20 14:55:15 +1000

    update build script

--- Examples/counter/build-counter.sh
+++ Examples/counter/build-counter.sh
@@ -24,9 +24,9 @@ cp ${FLUTTER_SDK}/bin/cache/artifacts/engine/linux-arm64/icudtl.dat \
    ${RESULT_DIR}/${BUILD_MODE}/bundle/data/
 
 # Build kernel_snapshot.
-${FLUTTER_SDK}/bin/cache/dart-sdk/bin/dart \
+${FLUTTER_SDK}/bin/cache/dart-sdk/bin/dartaotruntime \
   --verbose \
-  --disable-dart-dev ${FLUTTER_SDK}/bin/cache/artifacts/engine/linux-arm64/frontend_server.dart.snapshot \
+  --disable-dart-dev ${FLUTTER_SDK}/bin/cache/dart-sdk/bin/snapshots/frontend_server_aot.dart.snapshot \
   --sdk-root ${FLUTTER_SDK}/bin/cache/artifacts/engine/common/flutter_patched_sdk_product/ \
   --target=flutter \
   --no-print-incremental-dependencies \
@@ -34,18 +34,20 @@ ${FLUTTER_SDK}/bin/cache/dart-sdk/bin/dart \
   -Ddart.vm.product=true \
   --aot \
   --tfa \
+  --target-os linux \
   --packages .dart_tool/package_config.json \
   --output-dill .dart_tool/flutter_build/flutter-embedded-linux/app.dill \
   --depfile .dart_tool/flutter_build/flutter-embedded-linux/kernel_snapshot.d \
+  --verbosity=error \
   package:${APP_PACKAGE_NAME}/main.dart
 
 # Build AOT image.
 ${FLUTTER_SDK}/bin/cache/artifacts/engine/linux-arm64/gen_snapshot \
   --deterministic \
   --snapshot_kind=app-aot-elf \
-  --elf=.dart_tool/flutter_build/flutter-embedded-linux/libapp.so \
+  --elf=.dart_tool/flutter_build/flutter-embedded-linux/app.so \
+  --strip \
   .dart_tool/flutter_build/flutter-embedded-linux/app.dill
-#  --strip
 
-cp .dart_tool/flutter_build/flutter-embedded-linux/libapp.so ${RESULT_DIR}/${BUILD_MODE}/bundle/lib/
+cp .dart_tool/flutter_build/flutter-embedded-linux/app.so ${RESULT_DIR}/${BUILD_MODE}/bundle/lib/
 
