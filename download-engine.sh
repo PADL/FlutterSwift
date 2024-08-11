33a596fb8acacf8a3a8f99317de92554a8d0dd6f
Luke Howard <lukeh@padl.com>
2024-08-11 10:26:11 +1000

    update to Flutter 3.24

--- download-engine.sh
+++ download-engine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ENGINE_URL="https://github.com/sony/flutter-embedded-linux/releases/download/edd8546116"
+ENGINE_URL="https://github.com/sony/flutter-embedded-linux/releases/download/b8800d88be"
 
 tmp_dir=$(mktemp -d -t engine-XXXXXXXXXX)
 artifact_dir="$tmp_dir/flutter-engine.artifactbundle"
