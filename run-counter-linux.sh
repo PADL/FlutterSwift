42d9ed02756c4db68b6879db03134bc32240c6cf
Luke Howard <lukeh@padl.com>
2024-11-11 09:48:09 +1100

    fix shell script typo in Linux runner

--- run-counter-linux.sh
+++ run-counter-linux.sh
@@ -2,7 +2,7 @@
 pwd=`pwd`
 bundle="${pwd}/Examples/counter/build/elinux/arm64/debug/bundle"
 
-if [ "xFLUTTER_SWIFT_BACKEND" == "xwayland" ]; then
+if [ "x$FLUTTER_SWIFT_BACKEND" == "xwayland" ]; then
   .build/debug/counter ${bundle}
 else
   export FLUTTER_DRM_DEVICE=/dev/dri/card0
