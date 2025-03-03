e9712d34bc9a776276036a6f162f8f6326ecd323
Luke Howard <lukeh@padl.com>
2025-03-04 09:28:28 +1100

    Support for both debug and release builds in build scripts

---
+++ build-elinux.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+source build-defaults.inc
+
+pushd ${SOURCE_DIR}
+rm -rf ${BUNDLE_DIR}
+mkdir -p ${BUNDLE_DIR}/lib/
+mkdir -p ${BUNDLE_DIR}/data/
+
+mkdir -p .dart_tool/flutter_build/flutter-embedded-linux
+
+# Build Flutter assets.
+echo "Building Flutter assets..."
+${FLUTTER_ROOT}/bin/flutter-elinux build bundle --asset-dir=${BUNDLE_DIR}/data/flutter_assets
+
+cp ${FLUTTER_CACHE_ENGINEDIR}/linux-${ARCH}/icudtl.dat ${BUNDLE_DIR}/data/
+
+# Build kernel_snapshot.
+echo "Building kernel snapshot..."
+${DART_CACHE_BINDIR}/dartaotruntime \
+  --verbose \
+  --disable-dart-dev ${DART_CACHE_BINDIR}/snapshots/frontend_server_aot.dart.snapshot \
+  --sdk-root ${FLUTTER_CACHE_ENGINEDIR}/common/flutter_patched_sdk_product/ \
+  --target=flutter \
+  --no-print-incremental-dependencies \
+  -Ddart.vm.profile=false \
+  -Ddart.vm.product=true \
+  --aot \
+  --tfa \
+  --target-os linux \
+  --packages .dart_tool/package_config.json \
+  --output-dill .dart_tool/flutter_build/flutter-embedded-linux/app.dill \
+  --depfile .dart_tool/flutter_build/flutter-embedded-linux/kernel_snapshot.d \
+  --verbosity=error \
+  package:${APP_PACKAGE_NAME}/main.dart
+
+# Build AOT image.
+echo "Building AOT image... "
+${FLUTTER_CACHE_ENGINEDIR}/linux-${ARCH}-release/gen_snapshot \
+  --deterministic \
+  --snapshot_kind=app-aot-elf \
+  --elf=.dart_tool/flutter_build/flutter-embedded-linux/app.so \
+  --strip \
+  .dart_tool/flutter_build/flutter-embedded-linux/app.dill
+
+cp .dart_tool/flutter_build/flutter-embedded-linux/app.so ${BUNDLE_DIR}/lib/libapp.so
+ls -al ${BUNDLE_DIR}/lib/libapp.so
+
+# remove these artefacts to ensure we don't accidentally start in JIT mode
+
+if [ "X${FLUTTER_SWIFT_BUILD_CONFIG}" == "Xrelease" ]; then
+  echo "Removing JIT artifacts..."
+  rm -rf ${BUNDLE_DIR}/data/flutter_assets/kernel_blob.bin
+  rm -rf ${BUNDLE_DIR}/data/flutter_assets/isolate_snapshot_data
+  rm -rf ${BUNDLE_DIR}/data/flutter_assets/vm_snapshot_data
+fi
+
+echo "Copying Flutter engine to bundle lib directory..."
+cp ${FLUTTER_CACHE_ENGINEDIR}/elinux-${ARCH}-${FLUTTER_SWIFT_BUILD_CONFIG}/libflutter_engine.so ${BUNDLE_DIR}/lib/
+
+echo "Building Swift component..."
+popd
+swift build --configuration ${FLUTTER_SWIFT_BUILD_CONFIG}
+
+echo "Done!"
---
+++ run-elinux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source build-defaults.inc
+
+if [ "x$FLUTTER_SWIFT_BACKEND" == "xwayland" ]; then
+  .build/$FLUTTER_SWIFT_BUILD_CONFIG/counter ${BUNDLE_DIR}
+else
+  export FLUTTER_DRM_DEVICE=/dev/dri/card0
+  sudo .build/$FLUTTER_SWIFT_BUILD_CONFIG/counter ${BUNDLE_DIR}
+fPWDi
