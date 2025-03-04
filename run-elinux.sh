83b47041a35d635cbdb71c511d3d0b9dbb02aa60
Luke Howard <lukeh@padl.com>
2025-03-04 11:38:21 +1100

    use cached libflutter_engine.so

--- run-elinux.sh
+++ run-elinux.sh
@@ -2,6 +2,12 @@
 
 source build-defaults.inc
 
+# prefer the cached release versions to the ones that are included in the
+# FlutterSwift zip (which are debug versions)
+
+export LD_LIBRARY_PATH="${FLUTTER_CACHE_ENGINEDIR}/elinux-${ARCH}-${FLUTTER_SWIFT_BUILD_CONFIG}"
+export LD_PRELOAD="${LD_LIBRARY_PATH}/libflutter_engine.so"
+
 if [ "x$FLUTTER_SWIFT_BACKEND" == "xwayland" ]; then
   .build/$FLUTTER_SWIFT_BUILD_CONFIG/counter ${BUNDLE_DIR}
 else
