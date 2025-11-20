b6dd869317a8f4c91e21d8e76d5d9c322a165934
Luke Howard <lukeh@padl.com>
2025-11-20 17:59:13 +1100

    Revert "use Swift 6 language mode on non-Darwin platforms"
    This reverts commit 505013ace0778c1b1241836d0a6715f8ca712722.

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ var targetDependencies = [Target.Dependency]()
 var targetPluginUsages = [Target.PluginUsage]()
 
 var platformCxxSettings: [CXXSetting] = []
-var platformSwiftSettings: [SwiftSetting] = [.swiftLanguageMode(.v5, .when(platforms: [.macOS, .iOS]))]
+var platformSwiftSettings: [SwiftSetting] = [.swiftLanguageMode(.v5)]
 
 let EnvSysRoot = ProcessInfo.processInfo.environment["SYSROOT"]
 
