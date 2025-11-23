c748d5e790085b5aa9ba4dbbc05ae35679727b80
Luke Howard <lukeh@padl.com>
2025-11-23 20:23:37 +1100

    add some documnetation on the platform thread

--- Sources/FlutterSwift/Messenger/FlutterBinaryMessenger.swift
+++ Sources/FlutterSwift/Messenger/FlutterBinaryMessenger.swift
@@ -37,6 +37,32 @@ public typealias FlutterPlatformThreadActor = UIThreadActor
 public typealias FlutterPlatformThreadActor = MainActor
 #endif
 
+//
+// https://docs.flutter.dev/platform-integration/platform-channels#channels-and-platform-threading
+//
+//   "When invoking channels on the platform side destined for Flutter,
+//    invoke them on the platform's main thread."
+//
+// Hence the send() methods in the protocol are annotated with
+// @FlutterPlatformThreadActor which will ensure they run on @MainActor on
+// Darwin, and @UIThreadActor on Android. This/ annotation ensures that the
+// caller doesn't need to be aware of the platform thread requirement.
+//
+// setMessageHandler() and cleanUp() are not annotated as such as they need
+// to be called from deinitializers, however setMessageHandler() is always
+// run on the platform thread at registration time because its only consumer
+// is a wrapper which is marked @FlutterPlatformThreadActor. This enables
+// channels to be registered in awakeFromNib() without needing to spawn a
+// task, which eliminates some race conditions.
+//
+// The basic, method and event channels themselves do not have actor
+// annotations. They are classes which are thread-safe (they use mutexes
+// and/or atomics). Their methods will switch to the platform actor when they
+// call into the common messenger implementation. The basic and method channels
+// are completely synchronous except for this call to send(). An analyis of
+// event channels is not provided here.
+//
+
 public protocol FlutterBinaryMessenger: Sendable {
   @FlutterPlatformThreadActor
   func send(on channel: String, message: Data?) throws
