19aa3215a78086c25f2e6aaed97dc18eac8cdf07
Luke Howard <lukeh@padl.com>
2026-01-30 10:53:29 +1100

    update eLinux flutter embedder to 3.38.3 (13e658725d)

--- run-elinux.sh
+++ run-elinux.sh
@@ -2,10 +2,7 @@
 
 source build-defaults.inc
 
-# prefer the cached release versions to the ones that are included in the
-# FlutterSwift zip (which are debug versions)
-
-export LD_LIBRARY_PATH="${FLUTTER_CACHE_ENGINEDIR}/elinux-${ARCH}-${FLUTTER_SWIFT_BUILD_CONFIG}"
+export LD_LIBRARY_PATH="${BUNDLE_DIR}/lib"
 export LD_PRELOAD="${LD_LIBRARY_PATH}/libflutter_engine.so"
 
 .build/$FLUTTER_SWIFT_BUILD_CONFIG/counter ${BUNDLE_DIR}
